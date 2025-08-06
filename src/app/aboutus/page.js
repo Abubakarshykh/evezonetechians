23cf9d82f3e051d3e9020295975d87246f1bd63f
Abubakarshykh <126658411+Abubakarshykh@users.noreply.github.com>
2025-08-06 06:52:37 -0700

    Update page.js
    comment error solved inside file of jsx abouts/page.js

--- src/app/aboutus/page.js
+++ src/app/aboutus/page.js
@@ -5,18 +5,19 @@ import Soldier from "@/Components/Soldier";
 import AbtRect from "@/Components/AbtRect";
 import Arrow from "@/Components/Arrow";
 
-
 export default function Page() {
   return (
     <>
-    <div className="bg-[#222222]">
-      <AboutHead />
-      <Line/>
-      <Soldier/>
-      <Arrow showOnAllScreens={true} />
-    //needed to hide the founding members for personal reasons
-      // <MeetTheLeaders/>
-      <AbtRect/>
+      <div className="bg-[#222222]">
+        <AboutHead />
+        <Line />
+        <Soldier />
+        <Arrow showOnAllScreens={true} />
+        
+        {/* needed to hide the founding members for personal reasons */}
+        {/* <MeetTheLeaders/> */}
+        
+        <AbtRect />
       </div>
     </>
   );
